cec18fb4f2984993a8f3ef03707116559340e67e
Ines Arous <ines@exascale.info>
2019-02-01 16:47:25 +0100

    ines:added model

---
+++ src/em_expl.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 28 18:21:41 2018
+
+@author: inesarous
+"""
+
+from load_data import LoadData
+import pandas as pd
+import csv
+import numpy as np
+from keras import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+
+
+class EM:
+
+    def __init__(self, influencer_x, annotation_matrix, infl2worker_label, worker2influencer_label,
+                 label_set, true_labels):
+        self.influencer_x = influencer_x
+        self.annotation_matrix = annotation_matrix
+        self.infl2worker_label = infl2worker_label
+        self.worker2influencer_label = worker2influencer_label
+        self.label_set = label_set
+        self.true_labels = true_labels
+
+    # initialization
+    def init_probabilities(self):
+        # initialize probability z_i (item's quality) randomly
+        p_z_i = np.random.randint(2, size=(len(self.infl2worker_label), 1))
+        # initialize probability phi_j (worker's reliability) randomly
+        p_phi_j = np.random.random((len(self.worker2influencer_label), 1))
+        return p_z_i, 1 - p_z_i, p_phi_j
+
+    def __sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def neural_network(self, input_layer):
+        theta_i = self.__sigmoid(np.dot(input_layer.astype(float), self.W_I))
+        return theta_i
+
+    # E-step
+    def Update_e2lpd(self):
+        self.e2lpd = {}
+        all_named_influencers = self.annotation_matrix[self.annotation_matrix.iloc[:, 2] == 1]
+        for infl in range(0, len(infl2worker_label)):
+            workers_naming_infl = all_named_influencers[all_named_influencers['influencer'] == infl].worker
+            updated_pz_1 = 1
+            updated_pz_0 = 1
+            for worker in range(0, workers_naming_infl.shape[0]):
+                updated_pz_1 = updated_pz_1 * self.theta_i[infl] * self.p_phi_j[int(workers_naming_infl.iloc[worker])]
+                updated_pz_0 = updated_pz_0 * (1 - self.theta_i[infl]) * (
+                            1 - self.p_phi_j[int(workers_naming_infl.iloc[worker])])
+            print("upz1", updated_pz_1, "upz0", updated_pz_0)
+            self.p_z_i_1[infl] = updated_pz_1 / (updated_pz_0 + updated_pz_1)
+            self.p_z_i_0[infl] = updated_pz_0 / (updated_pz_0 + updated_pz_1)
+        print "pz0=", self.p_z_i_0, "\npz1=", self.p_z_i_1
+        return self.p_z_i_0, self.p_z_i_1, self.theta_i
+
+    # M-step
+
+    def Update_phi_wi(self, classifier, eta=0.001):
+        # update theta_i
+
+        # Fitting the data to the training dataset
+        prob_e_step = np.where(self.p_z_i_0 > 0.5, 0, 1)
+        y = np.concatenate((self.true_labels[0:4, None], prob_e_step[4:]))
+        classifier.fit(self.influencer_x, y, epochs=100)
+        eval_model = classifier.evaluate(self.influencer_x, y)
+
+        print(eval_model)
+        print ("weights", classifier.get_weights())
+        self.theta_i = classifier.predict(self.influencer_x)
+        print("theta", self.theta_i)
+        for worker in range(0, len(self.worker2influencer_label)):
+            annotation_worker = self.annotation_matrix[self.annotation_matrix['worker'] == worker]
+            for infl in range(0, len(influencer_x)):
+                label_worker_infl = annotation_worker[annotation_worker['influencer'] == infl].label
+                if label_worker_infl.iloc[0] == 1:
+                    grad_phi = ((self.p_z_i_1[infl] / self.p_phi_j[worker]) - (
+                                self.p_z_i_0[infl] / (1 - self.p_phi_j[worker])))
+                else:
+                    grad_phi = ((self.p_z_i_0[infl] / self.p_phi_j[worker]) - (
+                                self.p_z_i_1[infl] / (1 - self.p_phi_j[worker])))
+
+                # eq 24 in the document
+                self.p_phi_j[worker] = self.p_phi_j[worker] + (eta * grad_phi)
+
+        return self.theta_i, self.p_phi_j, classifier
+
+    def run(self, iterr=20):
+        #self.p_z_i_0, self.p_z_i_1, self.p_phi_j = self.init_probabilities()
+        # initialization
+        self.p_phi_j = np.array([[0.1], [0.8], [0.4], [0.7], [0.6], [0.8], [0.8], [0.3], [0.6], [0.4], [0.7]])
+        self.p_z_i_0 = np.array([[1.0], [0.0], [1.0], [1.0], [0.0], [0.0], [0.0], [1.0]])
+        self.p_z_i_1 = 1 - self.p_z_i_0
+        classifier = Sequential()
+        # First Hidden Layer
+        layer0 = Dense(2, activation='sigmoid', kernel_initializer='random_normal',
+                       input_dim=self.influencer_x.shape[1])
+        classifier.add(layer0)
+        # Output Layer
+        layer1 = Dense(1, activation='sigmoid', kernel_initializer='random_normal')
+        classifier.add(layer1)
+        # Compiling the neural network
+        classifier.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+        y = np.concatenate((self.true_labels[0:4, None], self.p_z_i_1[4:]))
+        classifier.fit(self.influencer_x, y, epochs=100)
+        self.theta_i = classifier.predict(self.influencer_x)
+        print("theta", self.theta_i)
+        # self.theta_i=np.array([[0.61],[0.51],[0.613],[0.581 ],[0.61 ],[0.61],[0.58 ],[0.61]])
+        while iterr > 0:
+            # print(iterr)
+            # E-step
+            self.p_z_i_0, self.p_z_i_1, theta_i = self.Update_e2lpd()
+
+            # M-step
+            self.theta_i, self.p_phi_j, classifier = self.Update_phi_wi(classifier)
+
+            # compute the likelihood
+            # print (self.computelikelihood())
+            iterr -= 1
+
+        return self.p_z_i_0, self.p_z_i_1, self.theta_i, self.p_phi_j, classifier
+
+    def computelikelihood(self):
+
+        lh_1 = (self.p_z_i_0 * np.log(1 - self.p_phi_j)) + (self.p_z_i_1 * np.log(self.p_phi_j))
+        lh_2 = (self.p_z_i_0 * np.log(1 - self.theta_i)) + (self.p_z_i_1 * np.log(self.theta_i))
+
+        lh = lh_1 + lh_2
+
+        return lh
+
+
+def get_w2il_i2wl(datafile):
+    infl2worker_label = {}
+    worker2influencer_label = {}
+    label_set = []
+
+    f = open(datafile, 'r')
+    reader = csv.reader(f)
+    next(reader)
+
+    for line in reader:
+        worker, influencer, label = line
+        if influencer not in infl2worker_label:
+            infl2worker_label[influencer] = []
+
+        infl2worker_label[influencer].append([worker, label])
+        if worker not in worker2influencer_label:
+            worker2influencer_label[worker] = []
+        worker2influencer_label[worker].append([influencer, label])
+        if label not in label_set:
+            label_set.append(label)
+    return infl2worker_label, worker2influencer_label, label_set
+
+
+if __name__ == '__main__':
+    datafile = '../input/example_em.csv'
+    annotation_file = '../output/aij_em.csv'
+    ld = LoadData(datafile)
+
+    # extract the influencers features
+    answers = pd.read_csv(datafile).drop_duplicates(subset=['infl_acc'])
+    influencer_x = answers[['follower_nbr', 'followee_nbr']]
+
+    # standardizing the input feature
+    # sc = StandardScaler()
+    # influencer_x = sc.fit_transform(influencer_x)
+
+    # generate the annotation matrix, extract the influencers, the workers and the true labels
+    aij, all_workers, all_infl, true_labels = ld.generate_annotation_matrix_em(datafile)
+    # save teh annotation matrix
+    annotation_matrix = pd.DataFrame(data=aij, columns=['worker', 'influencer', 'label'])
+    annotation_matrix.to_csv(annotation_file, sep=",", index=False)
+
+    # generate two dictionnaries one for workers, one for influencers
+    infl2worker_label, worker2influencer_label, label_set = get_w2il_i2wl(annotation_file)
+
+    # call to EM
+    p_z_i_0, p_z_i_1, theta_i, p_phi_j, classifier = EM(influencer_x, annotation_matrix, infl2worker_label,
+                                                        worker2influencer_label, label_set, true_labels).run()
+
+    # #print the classification of named users
+    print(pd.DataFrame(data=np.concatenate([all_infl.reshape(all_infl.shape[0], 1), np.where(p_z_i_0 > 0.5, 0, 1),
+                                            true_labels.values.reshape(all_infl.shape[0], 1)], axis=1),
+                       columns=['influencer', 'classification', 'truth']))
---
+++ src/var_em_expl.py
@@ -0,0 +1,228 @@
+from load_data import LoadData
+import pandas as pd
+import csv
+import numpy as np
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Dense
+from math import floor
+from sklearn.preprocessing import StandardScaler
+from scipy.special import digamma
+from sklearn.preprocessing import StandardScaler
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+class EM_VI:
+    def __init__(self, worker_x, influencer_x, annotation_matrix, infl2worker_label, worker2influencer_label, label_set,
+                 true_labels):
+        self.worker_x = worker_x
+        self.influencer_x = influencer_x
+        self.annotation_matrix = annotation_matrix
+        self.infl2worker_label = infl2worker_label
+        self.worker2influencer_label = worker2influencer_label
+        self.label_set = label_set
+        self.true_labels = true_labels
+        self.reliability = self.init_reliability()
+        self.q_z_i_0 = np.random.randint(2, size=(len(self.infl2worker_label), 1)).astype(float)
+        self.q_z_i_1 = 1.0 - self.q_z_i_0
+        self.A = 8.0
+        self.B = 2.0
+        self.alpha, self.beta = self.init_alpha_beta()
+
+
+
+    def optimize_rj(self,x_train, n_neurons, nb_layers, training_epochs, display_step, batch_size, n_input, alpha, beta):
+        keep_prob = tf.placeholder(tf.float64)
+
+        # input layer
+        x = tf.placeholder(tf.float64, [None, n_input])
+        layer = x
+        # hideen layers
+        for _ in range(nb_layers):
+            layer = tf.layers.dense(inputs=layer, units=n_neurons, activation=tf.nn.tanh)
+        # output layer
+        alpha_prime = tf.layers.dense(inputs=layer, units=1, activation=lambda x: tf.nn.relu(x) + 1)
+        beta_prime = tf.layers.dense(inputs=layer, units=1, activation=lambda x: tf.nn.relu(x) + 1)
+
+        dist = tf.distributions.Beta(alpha_prime, beta_prime)
+        target_dist = tf.distributions.Beta(alpha, beta)
+
+        loss = tf.distributions.kl_divergence(target_dist, dist)
+        cost = tf.reduce_mean(loss)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for epoch in range(training_epochs):
+                avg_cost = 0.0
+                total_batch = int(len(x_train) / batch_size)
+                x_batches = np.array_split(x_train, total_batch)
+                for i in range(total_batch):
+                    batch_x = x_batches[i]
+                    _, c = sess.run([optimizer, cost],
+                                    feed_dict={
+                                        x: batch_x,
+                                        keep_prob: 0.8
+                                    })
+                    avg_cost += c / total_batch
+                if epoch % display_step == 0:
+                    print("Epoch:", '%04d' % (epoch + 1), "cost=", \
+                          "{:.9f}".format(avg_cost))
+            print("Optimization Finished!")
+
+            alpha_prime_res, beta_prime_res = sess.run([alpha_prime, beta_prime],
+                                                       feed_dict={
+                                                           x: x_train,
+                                                           keep_prob: 0.8
+                                                       })
+            print("alpha_prime_res=", alpha_prime_res, "beta_prime_res=", beta_prime_res)
+            return alpha_prime_res, beta_prime_res
+
+    def init_reliability(self):
+        reliability = np.zeros((len(self.worker2influencer_label), 1))
+        for w in range(0, len(self.worker2influencer_label)):
+            total_w_correct_answers = sum(
+                np.logical_and(np.array(self.worker2influencer_label.get(w))[:, 1], true_labels.values))
+            total_w_answers = sum(np.array(self.worker2influencer_label.get(w))[:, 1])
+            reliability[w] = float(total_w_correct_answers) / float(total_w_answers)
+        return reliability
+
+    def init_theta_i(self):
+        # learn theta_i
+        classifier = Sequential()
+        # first hidden layer
+        layer0 = Dense(3, activation='sigmoid', kernel_initializer='random_normal',
+                       input_dim=self.influencer_x.shape[1])
+        classifier.add(layer0)
+        # Output Layer
+        layer1 = Dense(1, activation='sigmoid', kernel_initializer='random_normal')
+        classifier.add(layer1)
+        # Compiling the neural network
+        classifier.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+        y = np.concatenate((self.true_labels[0:int(len(self.infl2worker_label) / 3), None],
+                            self.q_z_i_1[int(len(infl2worker_label) / 3):]))
+        classifier.fit(self.influencer_x, y, epochs=100)
+        theta_i = classifier.predict(self.influencer_x)
+        return theta_i, classifier
+
+    def init_alpha_beta(self):
+        alpha =  np.zeros((len(worker2influencer_label),1))
+        beta =  np.zeros((len(worker2influencer_label),1))
+        for w in range(0, len(self.worker2influencer_label)):
+            alpha[w] = self.A
+            beta[w] = self.B
+        return alpha, beta
+
+    def update(self, a, b):
+        self.n_update += 1
+        self.change += np.abs(a - b)
+
+    def e_step(self, max_it=10):
+        for it in range(max_it):
+            self.change = 0
+            self.n_update = 0
+            # update q(z)
+            for influencer in range(0, len(infl2worker_label)):
+                q_z_i_0 = 1
+                q_z_i_1 = 1
+                infl_aij=self.annotation_matrix[self.annotation_matrix['influencer'] == influencer]
+                T_i=infl_aij[infl_aij['label'] == 1].worker.values
+                for worker in T_i.astype(int):
+                    alpha = self.alpha[worker]
+                    beta = self.beta[worker]
+                    q_z_i_0 = q_z_i_0 * (1 - self.theta_i[influencer]) * np.exp(digamma(beta) - digamma(alpha + beta))
+                    q_z_i_1 = q_z_i_1 * self.theta_i[influencer] * np.exp(digamma(alpha) - digamma(alpha + beta))
+                new_q_z_i_1 = q_z_i_1 * 1.0 / (q_z_i_0 + q_z_i_1)
+                self.update(self.q_z_i_1[influencer], new_q_z_i_1)
+                self.q_z_i_0[influencer] = q_z_i_0 * 1.0 / (q_z_i_0 + q_z_i_1)
+                self.q_z_i_1[influencer] = q_z_i_1 * 1.0 / (q_z_i_0 + q_z_i_1)
+
+            # update q(r)
+            new_alpha = np.zeros((len(worker2influencer_label),1))
+            new_beta = np.zeros((len(worker2influencer_label),1))
+            for worker in range(0, len(self.worker2influencer_label)):
+                new_alpha[worker] = self.A
+                new_beta[worker] = self.B
+
+            for worker in range(0, len(self.worker2influencer_label)):
+                worker_aij=self.annotation_matrix[self.annotation_matrix['worker'] == worker]
+                T_j=worker_aij[worker_aij['label'] == 1].influencer.values
+                for infl in T_j.astype(int):
+                    new_alpha[worker] += (1 - self.theta_i[infl])
+                    new_beta[worker] += self.theta_i[infl]
+            for worker in range(0, len(self.worker2influencer_label)):
+                self.update(self.alpha[worker], new_alpha[worker])
+                self.alpha[worker] = new_alpha[worker]
+                self.update(self.beta[worker], new_beta[worker])
+                self.beta[worker] = new_beta[worker]
+            avg_change = self.change * 1.0 / self.n_update
+            if avg_change < 0.01:
+                break
+
+    def m_step(self, classifier):
+        prob_e_step = np.where(self.q_z_i_0 > 0.1, 0, 1)
+        print prob_e_step
+        y = np.concatenate((self.true_labels[0:int(len(self.infl2worker_label) / 3), None],
+                            prob_e_step[int(len(infl2worker_label) / 3):]))
+        classifier.fit(self.influencer_x, y, epochs=100)
+        self.theta_i = classifier.predict(self.influencer_x)
+        n_neurons = 3
+        nb_layers = 2
+        training_epochs = 100
+        display_step = 10
+        batch_size = 1
+        n_input = self.worker_x.shape[1]
+        alpha_prime_res, beta_prime_res = self.optimize_rj(self.worker_x, n_neurons, nb_layers, training_epochs, display_step,
+                                                      batch_size, n_input, self.alpha, self.beta)
+        self.alpha = alpha_prime_res
+        self.beta = beta_prime_res
+
+
+    def run(self, iterr=20):
+        self.theta_i, classifier = self.init_theta_i()
+        while iterr > 0:
+            # variational E step
+            self.e_step()
+        #     # variational M step
+            self.m_step(classifier)
+            iterr -= 1
+        return self.q_z_i_0, self.q_z_i_1, self.theta_i, self.reliability, self.alpha, self.beta
+
+
+def get_w2il_i2wl(datafile):
+    infl2worker_label = {}
+    worker2influencer_label = {}
+    label_set = []
+    f = open(datafile, 'r')
+    reader = csv.reader(f)
+    next(reader)
+    for line in reader:
+        w, infl, lbl = line
+        worker = float(w)
+        influencer = float(infl)
+        label = float(lbl)
+        # generate dictionnary for influencers
+        if influencer not in infl2worker_label:
+            infl2worker_label[influencer] = []
+        infl2worker_label[influencer].append([int(worker), int(label)])
+        # generate dictionnary for workers
+        if worker not in worker2influencer_label:
+            worker2influencer_label[worker] = []
+        worker2influencer_label[worker].append([int(influencer), int(label)])
+        if label not in label_set:
+            label_set.append(int(label))
+    return infl2worker_label, worker2influencer_label, label_set
+
+
+if __name__ == '__main__':
+    worker_x = pd.read_csv('../input/worker_x.csv')
+    influencer_x = pd.read_csv('../input/influencer_x.csv')
+    true_labels = pd.read_csv('../input/labels_infl.csv').label
+    annotation_matrix =  pd.read_csv('../output/aij.csv')
+    # sc = StandardScaler()
+    # influencer_x = sc.fit_transform(influencer_x)
+    # worker_x = sc.fit_transform(worker_x)
+    infl2worker_label, worker2influencer_label, label_set = get_w2il_i2wl('../output/aij.csv')
+    qz0, qz1, theta_i, rj, alpha, beta = EM_VI(worker_x, influencer_x, annotation_matrix, infl2worker_label,
+                                               worker2influencer_label, label_set, true_labels).run()
+    print(pd.DataFrame(data=np.concatenate([ np.where(qz0 > 0.5, 0, 1),
+                                            true_labels.values.reshape(true_labels.shape[0], 1)], axis=1),columns=['classification', 'truth']))
